d3af2ce8c39e4257c0d23686e95925f3542c74c3
Aman4922 <140258524+Aman4922@users.noreply.github.com>
2025-01-19 07:59:40 +0000

    contianer config done

---
+++ .devcontainer/01_basicss/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";// treat all Js code as newer version 
+//alert(3+3)// we are using nodejs,not browser
+let name='aman'
+let age=15
+let isLoggedIn=false
+
+/*
+number =2^53
+bigInt
+String=""
+boolean=ttrue/false
+null=standalone value
+undefined=means value is not assigned 
+symbol=unique
+*/
+console.log(typeof age);
+console.log(typeof null);//here the output is giving object 
+console.log(typeof undefined);//here the output is giving undefined because it is a data type 
