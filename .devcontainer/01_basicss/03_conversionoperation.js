75dd5e29859c709380c5477d5fcccd4141e8e0ed
Aman4922 <140258524+Aman4922@users.noreply.github.com>
2025-01-20 05:47:50 +0000

    container config done

---
+++ .devcontainer/01_basicss/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score ="33abc"
+
+console.log(typeof score)
+let  valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+/*
+"33"=33
+"33abc=NaN"
+true=1;false=0
+*/
+
+let isLoggedIn=""
+let booleanIsLogeedIn=Boolean(isLoggedIn)
+console.log(booleanIsLogeedIn)
+
+//note
+// 1= true;0=false
+//""=false
+//"name"=true
+
+let someNumber=333
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
